8ceeefd6d1aca6cc5cd50bb182ac0d727a75189c
Haizhao Yang <haizhaoyang@yahoo.com>
2018-08-22 20:00:37 +0800

    revise ampltitude estimation

--- Source/GeneralModeDecom/Utilities/amplt_est.m
+++ Source/GeneralModeDecom/Utilities/amplt_est.m
@@ -1,5 +1,5 @@
 function amplt = amplt_est(is_real,y)
-% Estimate instantaneous frequency by computing analytic signals
+% Estimate 1D instantaneous amplitude by computing analytic signals
 %
 % By Haizhao Yang
 
@@ -7,7 +7,7 @@ if is_real
     yh = fftshift(fft(ifftshift(y)));
     yh(1:floor(end/2)) = 0;
     yh(floor(end/2)+1:end) = 2*yh(floor(end/2)+1:end);
-    y = fftshift(ifft2(ifftshift(yh)));
+    y = fftshift(ifft(ifftshift(yh)));
     amplt = abs(y);
 else
     amplt = abs(y);
